59a49943fa1755e0b05884e7ae60e5b76d751773
Arthur <arthur@comets.kr>
2023-07-27 11:57:31 +0900

    서버 실행 bat 수정

--- start-server-dev.bat
+++ start-server-dev.bat
@@ -1,7 +1,7 @@
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev api-gateway
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev graphql-gateway
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev user
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev board
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev auth
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev redis-cache
-start /d "D:\Web\Tests\msa-nestjs" /b npm run start:dev chat
+start /d ".\" /b npm run start:dev api-gateway
+start /d ".\" /b npm run start:dev graphql-gateway
+start /d ".\" /b npm run start:dev user
+start /d ".\" /b npm run start:dev board
+start /d ".\" /b npm run start:dev auth
+start /d ".\" /b npm run start:dev redis-cache
+start /d ".\" /b npm run start:dev chat
